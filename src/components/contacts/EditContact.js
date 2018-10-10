10e61292d717c09022ffd922984b77b428dfa8f1
Jurek <jurek2006@gmail.com>
2018-10-10 15:44:03 +0200

    51. Get Single Contact For Edit

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -1,101 +1,126 @@
-import React, { Component } from 'react';
-import TextInputGroup from '../layout/TextInputGroup';
+import React, { Component } from "react";
+import TextInputGroup from "../layout/TextInputGroup";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getContact } from "../../actions/contactActions";
 
 class EditContact extends Component {
-  state = {
-    name: '',
-    email: '',
-    phone: '',
-    errors: {}
-  };
-
-  onSubmit = (e) => {
-    e.preventDefault();
-
-    const { name, email, phone } = this.state;
-
-    // Check For Errors
-    if (name === '') {
-      this.setState({ errors: { name: 'Name is required' } });
-      return;
-    }
-
-    if (email === '') {
-      this.setState({ errors: { email: 'Email is required' } });
-      return;
-    }
-
-    if (phone === '') {
-      this.setState({ errors: { phone: 'Phone is required' } });
-      return;
-    }
-
-    const updContact = {
-      name,
-      email,
-      phone
+    state = {
+        name: "",
+        email: "",
+        phone: "",
+        errors: {}
     };
 
-    const { id } = this.props.match.params;
+    // componentWillReceiveProps(nextProps, nextState) {
+    //     const { name, email, phone } = nextProps.contact;
+    //     this.setState({
+    //         name,
+    //         email,
+    //         phone
+    //     });
+    // }
 
-    //// UPDATE CONTACT ////
+    componentDidUpdate(props, state, snapshot) {
+        if (this.props.contact !== props.contact) {
+            this.setState(this.props.contact);
+        }
+    }
 
-    // Clear State
-    this.setState({
-      name: '',
-      email: '',
-      phone: '',
-      errors: {}
-    });
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        this.props.getContact(id);
+    }
 
-    this.props.history.push('/');
-  };
+    onSubmit = e => {
+        e.preventDefault();
 
-  onChange = e => this.setState({ [e.target.name]: e.target.value });
+        const { name, email, phone } = this.state;
 
-  render() {
-    const { name, email, phone, errors } = this.state;
+        // Check For Errors
+        if (name === "") {
+            this.setState({ errors: { name: "Name is required" } });
+            return;
+        }
 
-    return (
-      <div className="card mb-3">
-        <div className="card-header">Edit Contact</div>
-        <div className="card-body">
-          <form onSubmit={this.onSubmit}>
-            <TextInputGroup
-              label="Name"
-              name="name"
-              placeholder="Enter Name"
-              value={name}
-              onChange={this.onChange}
-              error={errors.name}
-            />
-            <TextInputGroup
-              label="Email"
-              name="email"
-              type="email"
-              placeholder="Enter Email"
-              value={email}
-              onChange={this.onChange}
-              error={errors.email}
-            />
-            <TextInputGroup
-              label="Phone"
-              name="phone"
-              placeholder="Enter Phone"
-              value={phone}
-              onChange={this.onChange}
-              error={errors.phone}
-            />
-            <input
-              type="submit"
-              value="Update Contact"
-              className="btn btn-light btn-block"
-            />
-          </form>
-        </div>
-      </div>
-    );
-  }
+        if (email === "") {
+            this.setState({ errors: { email: "Email is required" } });
+            return;
+        }
+
+        if (phone === "") {
+            this.setState({ errors: { phone: "Phone is required" } });
+            return;
+        }
+
+        // Clear State
+        this.setState({
+            name: "",
+            email: "",
+            phone: "",
+            errors: {}
+        });
+
+        this.props.history.push("/");
+    };
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+        const { name, email, phone, errors } = this.state;
+
+        return (
+            <div className="card mb-3">
+                <div className="card-header">Edit Contact</div>
+                <div className="card-body">
+                    <form onSubmit={this.onSubmit}>
+                        <TextInputGroup
+                            label="Name"
+                            name="name"
+                            placeholder="Enter Name"
+                            value={name}
+                            onChange={this.onChange}
+                            error={errors.name}
+                        />
+                        <TextInputGroup
+                            label="Email"
+                            name="email"
+                            type="email"
+                            placeholder="Enter Email"
+                            value={email}
+                            onChange={this.onChange}
+                            error={errors.email}
+                        />
+                        <TextInputGroup
+                            label="Phone"
+                            name="phone"
+                            placeholder="Enter Phone"
+                            value={phone}
+                            onChange={this.onChange}
+                            error={errors.phone}
+                        />
+                        <input
+                            type="submit"
+                            value="Update Contact"
+                            className="btn btn-light btn-block"
+                        />
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default EditContact;
+EditContact.propTypes = {
+    contact: PropTypes.object.isRequired,
+    getContact: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+    contact: state.contact.contact
+});
+
+export default connect(
+    mapStateToProps,
+    { getContact }
+)(EditContact);
